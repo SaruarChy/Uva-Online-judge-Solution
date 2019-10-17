92d130827b10f6612602461efd4659f8ca4d0222
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-10-18 00:22:32 +0600

    Add files via upload

---
+++ 10970- Big Chocolate.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long  int m,n,f;
+    while(scanf("%ld%ld", &m,&n)!=EOF)
+    {
+        f=m*n-1;
+        cout<<f<<endl;
+    }
+
+    return 0;
+}
