41e877e451efb459142207c255cb366e651884d3
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-10-18 00:02:28 +0600

    Add files via upload

---
+++ 12646- Zero or One.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    while(scanf("%d %d %d", &a, &b, &c)==3)
+    {
+        if(a==0 && b==0 && c==0)
+            printf("*\n");
+        else if (a==1 && b==0 && c==0)
+            printf("A\n");
+        else if (a==0 && b==1 && c==0)
+            printf("B\n");
+        else if (a==0 && b==0 && c==1)
+            printf("C\n");
+        else if(a==0 && b==1 && c==1)
+            printf("A\n");
+        else if (a==1 && b==0 && c==1)
+            printf("B\n");
+        else if (a==1 && b==1 && c==0)
+            printf("C\n");
+        else
+            printf("*\n");
+    }
+    return 0;
+}
