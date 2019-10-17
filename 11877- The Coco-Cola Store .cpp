263bb8da105cd05137ededa87a9612a2514a3d33
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-10-17 23:51:13 +0600

    Add files via upload

---
+++ 11877- The Coco-Cola Store .cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n, count=1;
+    while(count<=10){
+            scanf("\n%d", &n);
+            if(n==0)
+                break;
+        ++count;
+
+    m=n/2;
+    cout<<m<<endl;
+    }
+    return 0;
+}
