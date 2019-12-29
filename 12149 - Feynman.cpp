6020cd2ce1be131ae5e1e53a9900d2eaaaa7bb89
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2019-12-30 01:46:57 +0600

    Math

---
+++ 12149 - Feynman.cpp
@@ -0,0 +1,14 @@
+//Bismillahir Rahmanir Rahim
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while(1)
+    {
+        cin>>n;
+        if(n==0)break;
+        cout<<n*(n+1)*(2*n+1)/6<<endl;
+    }
+    return 0;
+}
